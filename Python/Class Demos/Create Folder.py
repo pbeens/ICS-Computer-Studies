e1360fdf5939e333587c27434934bf6a162e58fd
pbeens <Peter.Beens@dsbn.org>
2016-10-05 11:00:28 -0400

    Updated Create Folder demo

--- Python/Class Demos/Create Folder.py
+++ Python/Class Demos/Create Folder.py
@@ -1,15 +1,26 @@
+'''
+Sometimes in class you will be asked to put the output from your program in a personalized subfolder, perhaps named
+with your name or your initials.
+
+This example shows you how you can create the folder, as well as delete the folder and its contents if it already exists.
+'''
+
 # import(s)
 import os
+import shutil
 
 # declarations
 folderName = "beens"
-fileName = folderName + '/' + 'test.txt'
 
-# create the folder (test if exists first!)
-if not os.path.exists(folderName):
-    os.makedirs(folderName)
+# delete the folder and its contents if it aleady exists
+if os.path.exists(folderName):
+    shutil.rmtree(folderName)
+
+# create the folder
+os.makedirs(folderName)
 
 # write a file in the folder
-file = open(fileName, 'w')
+fileName = 'test.txt'
+file = open(folderName + '/' + fileName, 'w')
 file.write("this is data in the text file")
 file.close()
