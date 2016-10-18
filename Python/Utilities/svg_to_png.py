5a0c359d6f2b313d4b46e5d5ac880a5d3a36ecf1
pbeens <Peter.Beens@dsbn.org>
2016-10-18 15:41:12 -0400

    Added SVG_to_PNG utility

---
+++ Python/Utilities/svg_to_png.py
@@ -0,0 +1,18 @@
+'''
+Program to convert all SVG files in the current folder to PNG.
+
+Bug: drawing must be within page boundaries 
+'''
+
+from os import walk
+import subprocess
+
+# put all filenames in the current directory in the variable filenames
+for (dirpath, dirnames, filenames) in walk("."):
+    break
+
+# convert each file in filenames from svg to png
+for file in filenames:
+    if file.endswith("svg"):
+        subprocess.Popen(['C:\\Program Files (x86)\\Inkscape\\Inkscape.exe', "-w1024 --export-area-drawing -f", file, "-e", file[:-4] + ".png"])
+
